ec3683d589de46941aa812c2f4f61b767377ce64
Mustafa Culha <merguven753@gmail.com>
2023-04-14 10:39:15 +0300

    Day 14.04.2023

--- src/test/java/hooks/api/HooksAPI.java
+++ src/test/java/hooks/api/HooksAPI.java
@@ -3,9 +3,9 @@ package hooks.api;
 public class HooksAPI {
 
 
-     README.md
-    git commit -m "first commit"
-    git branch -M main
-    git remote add origin https://github.com/culhamu/Cucumber_API.git
-    git push -u origin main
+
+
+
+
+
 }
