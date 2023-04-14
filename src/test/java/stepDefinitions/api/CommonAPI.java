0ac2e81c895744c3106cf8c3a528d9fc0e4bad18
Mustafa Culha <merguven753@gmail.com>
2023-04-14 11:47:19 +0300

    Day 14.04.2023

--- src/test/java/stepDefinitions/api/CommonAPI.java
+++ src/test/java/stepDefinitions/api/CommonAPI.java
@@ -1,4 +1,40 @@
 package stepDefinitions.api;
 
+import hooks.api.HooksAPI;
+import io.cucumber.java.en.Given;
+
 public class CommonAPI {
+
+   public static String fullPath;
+
+    @Given("Api kullanicisi {string} path parametrelerini set eder")
+    public void api_kullanicisi_path_parametrelerini_set_eder(String rawPath) {
+
+        //https://trendlifebuy.com/api/register
+
+        //api/register
+
+        String[] paths=rawPath.split("/");//["api","register"]
+
+
+        StringBuilder tempPath=new StringBuilder("{");
+
+        for (int i = 0; i <paths.length ; i++) {
+
+            String key="pp"+i;//pp0 pp1 pp2
+
+            String value=paths[i].trim();
+
+            HooksAPI.spec.pathParams(key,value);
+
+            tempPath.append(key+"}/{");
+
+        }
+
+        tempPath.deleteCharAt(tempPath.lastIndexOf("{"));
+        tempPath.deleteCharAt(tempPath.lastIndexOf("/"));
+
+        fullPath=tempPath.toString();
+
+    }
 }
---
+++ src/test/resources/features/api/Post_RegisterCustomer.feature
@@ -0,0 +1,7 @@
+@API
+
+  Feature: Used to create a new user
+
+    Scenario: Success Response
+
+      Given Api kullanicisi "api/register" path parametrelerini set eder
