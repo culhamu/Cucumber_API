5d20f51072012b23851e85b6f9410ae29a7fa7c1
Mustafa Culha <merguven753@gmail.com>
2023-04-14 12:34:28 +0300

    Day 14.04.2023

--- src/test/java/stepDefinitions/api/CommonAPI.java
+++ src/test/java/stepDefinitions/api/CommonAPI.java
@@ -2,10 +2,19 @@ package stepDefinitions.api;
 
 import hooks.api.HooksAPI;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import pojos.Pojo_RegisterCustomer;
+import utilities.ConfigReader;
+
+import static io.restassured.RestAssured.given;
 
 public class CommonAPI {
 
    public static String fullPath;
+
+   public static Pojo_RegisterCustomer requestBody;
 
     @Given("Api kullanicisi {string} path parametrelerini set eder")
     public void api_kullanicisi_path_parametrelerini_set_eder(String rawPath) {
@@ -34,7 +43,38 @@ public class CommonAPI {
         tempPath.deleteCharAt(tempPath.lastIndexOf("{"));
         tempPath.deleteCharAt(tempPath.lastIndexOf("/"));
 
-        fullPath=tempPath.toString();
+        fullPath=tempPath.toString();//  /{pp0}/{pp1}/{pp2}
 
     }
+
+    @Then("Register Customer icin gerekli Request Body {string},{string},{string},{string},{string},{string},{string},{string},{string} hazirla")
+    public void register_customer_icin_gerekli_request_body_hazirla(String first_name, String last_name, String username, String email, String password, String password_confirmation, String user_type, String phone, String referral_code) {
+
+        //{
+        //  "first_name": "sdad",
+        //  "last_name": "sdsd",
+        //  "username":"sdsadas",
+        //  "email": "daasdasdss@gmail.com",
+        //  "password": "As.123123",
+        //  "password_confirmation": "As.123123",
+        //  "user_type": "customer",
+        //  "phone":"121123123",
+        //  "referral_code": "44546545464546"
+        //}
+
+      requestBody=new Pojo_RegisterCustomer(first_name,last_name,username,email,password,password_confirmation,user_type,phone,referral_code);
+
+    }
+
+    @Then("Register Customer icin Post request gonder")
+    public void register_customer_icin_post_request_gonder() {
+
+        Response response=given().spec(HooksAPI.spec).contentType(ContentType.JSON)
+                .when().body(requestBody)
+                .post(fullPath);
+
+        response.prettyPeek();
+
+    }
+
 }
--- src/test/resources/features/api/Post_RegisterCustomer.feature
+++ src/test/resources/features/api/Post_RegisterCustomer.feature
@@ -5,3 +5,5 @@
     Scenario: Success Response
 
       Given Api kullanicisi "api/register" path parametrelerini set eder
+      Then Register Customer icin gerekli Request Body "Mustafa","Talebe","Mustafa","mustafa@mail.com","12345678","12345678","customer","6547893218","44546545464546" hazirla
+      Then Register Customer icin Post request gonder
